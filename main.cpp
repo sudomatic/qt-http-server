2cdce7c2e07f0487fcb3553d46d05dc9712ac1dd
Thomas BOUTROUE <thebootroo@gmail.com>
2014-07-19 16:46:03 +0200

    basic example of url param parsing in test app

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QStringBuilder>
+#include <QUrlQuery>
+#include <QList>
+#include <QPair>
 
 #include "QtHttpServer.h"
 #include "QtHttpRequest.h"
@@ -37,13 +40,31 @@ static void onRequestNeedsReply (QtHttpRequest * request, QtHttpReply * reply) {
     reply->appendRawData (QByteArrayLiteral ("<html>"));
     reply->appendRawData (QByteArrayLiteral ("<head>"));
     reply->appendRawData (QByteArrayLiteral ("<title>Testing Qt5 HTTP Server</title>"));
-    reply->appendRawData (QByteArrayLiteral ("<style>pre { border: 1px solid darkgray; background: lightgray; padding: 10px; }</style>"));
+    reply->appendRawData (QByteArrayLiteral ("<style>pre { border: 1px solid darkgray; background: lightgray; padding: 10px; } td,th { border: 1px solid gray; text-align: center; }</style>"));
     reply->appendRawData (QByteArrayLiteral ("</head>"));
-    reply->appendRawData (QByteArrayLiteral ("<body>"));
+    reply->appendRawData (QByteArrayLiteral ("<body style='margin: 50px'>"));
     reply->appendRawData (QByteArrayLiteral ("<h1>It Works !</h1>\r\n"));
     reply->appendRawData (QByteArrayLiteral ("<hr />\r\n"));
     reply->appendRawData (QByteArrayLiteral ("<h2>HTTP command: ")       % request->getCommand ().toUtf8 ()             % QByteArrayLiteral ("</h2>\r\n"));
     reply->appendRawData (QByteArrayLiteral ("<h3>Requested URL: ")      % request->getUrl ().toString ().toUtf8 ()     % QByteArrayLiteral ("</h3>\r\n"));
+    reply->appendRawData (QByteArrayLiteral ("<table style='border: 1px solid gray'>"));
+    reply->appendRawData (QByteArrayLiteral ("<tr>"));
+    reply->appendRawData (QByteArrayLiteral ("<th colspan='2'>URL parameters</th>"));
+    reply->appendRawData (QByteArrayLiteral ("</tr>"));
+    reply->appendRawData (QByteArrayLiteral ("<tr>"));
+    reply->appendRawData (QByteArrayLiteral ("<th>Key</th>"));
+    reply->appendRawData (QByteArrayLiteral ("<th>Value</th>"));
+    reply->appendRawData (QByteArrayLiteral ("</tr>"));
+    QUrlQuery params (request->getUrl ());
+    QList<QPair<QString, QString> > pairsList = params.queryItems ();
+    for (int idx = 0; idx < pairsList.size (); idx++) {
+        QPair<QString, QString> pair = pairsList.at (idx);
+        reply->appendRawData (QByteArrayLiteral ("<tr>"));
+        reply->appendRawData (QByteArrayLiteral ("<td>") % pair.first.toUtf8 () % QByteArrayLiteral ("</td>"));
+        reply->appendRawData (QByteArrayLiteral ("<td>") % pair.second.toUtf8 () % QByteArrayLiteral ("</td>"));
+        reply->appendRawData (QByteArrayLiteral ("</tr>"));
+    }
+    reply->appendRawData (QByteArrayLiteral ("</table>"));
     reply->appendRawData (QByteArrayLiteral ("<h4>Virtual host: ")       % request->getHeader (QtHttpHeader::Host)      % QByteArrayLiteral ("</h4>\r\n"));
     reply->appendRawData (QByteArrayLiteral ("<h5>Your User-Agent: ")    % request->getHeader (QtHttpHeader::UserAgent) % QByteArrayLiteral ("</h5>\r\n"));
     reply->appendRawData (QByteArrayLiteral ("<h6>Request raw data:\r\n</h6>"));
